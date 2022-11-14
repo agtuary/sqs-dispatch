d57c76e747c7698dbe1b013eb939f9dbce507f9e
Ray Yamamoto Hilton <ray.hilton@bluejay-ai.com>
2022-11-14 14:28:15 +1100

    Fix module name

--- sqs-dispatch/worker.py
+++
@@ -1,9 +0,0 @@
-from .queue import process_queue
-from .command import execute
-import click
-import logging
-import json
-
-
-async def run_worker(queue: str):
-    await process_queue(queue, lambda x: execute(x["command"]))
