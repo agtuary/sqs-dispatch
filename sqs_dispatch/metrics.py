fd2b58a7dcbf01e1d983c6fe270d66aaaf1685bc
Ray Yamamoto Hilton <ray.hilton@bluejay-ai.com>
2022-11-23 14:55:38 +1100

    Reinstate exception handling

--- sqs_dispatch/metrics.py
+++ sqs_dispatch/metrics.py
@@ -9,27 +9,25 @@ logger = logging.getLogger(__name__)
 
 
 async def send_finished_metrics(event, duration, metric_tags={}):
-    # try:
-    api.Metric.send(
-        metrics=[
-            {
-                "metric": f"ag.sqs_dispatch.task.{event}",
-                "type": "count",
-                "points": [(int(time()), 1)],
-                "tags": metric_tags,
-            },
-            {
-                "metric": "ag.sqs_dispatch.task.duration",
-                "type": "count",
-                "points": [(int(time()), duration)],
-                "tags": metric_tags,
-            },
-        ]
-    )
-
-
-# except api.exceptions.ApiNotInitialized:
-#     logger.warning("Datadog API not initialized, not sending metrics")
+    try:
+        api.Metric.send(
+            metrics=[
+                {
+                    "metric": f"ag.sqs_dispatch.task.{event}",
+                    "type": "count",
+                    "points": [(int(time()), 1)],
+                    "tags": metric_tags,
+                },
+                {
+                    "metric": "ag.sqs_dispatch.task.duration",
+                    "type": "count",
+                    "points": [(int(time()), duration)],
+                    "tags": metric_tags,
+                },
+            ]
+        )
+    except api.exceptions.ApiNotInitialized:
+        logger.warning("Datadog API not initialized, not sending metrics")
 
 
 @asynccontextmanager
